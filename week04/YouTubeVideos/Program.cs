14249a4c9222812ee315b8431004149cc72b34cb
Skyyzg15 <cielozegarragarcia@gmail.com>
2025-10-11 18:55:46 -0500

    finished the project

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        var video1 = new Video("How to make a website using HTML and CSS?", "Alice", 300);
+        video1.AddComment(new Comment("Diego", "This is a great video!"));
+        video1.AddComment(new Comment("Rosa", "Amazing!!"));
+        video1.AddComment(new Comment("John", "Fantastic"));
+
+        var video2 = new Video("Understanding C#", "Eve", 400);
+        video2.AddComment(new Comment("Sarah", "Awesomee!"));
+        video2.AddComment(new Comment("Rosa", "Incredible video"));
+        video2.AddComment(new Comment("Nicole", "Wowww"));
+
+        var video3 = new Video("Mastering HTML", "Christian" 500);
+        video3.AddComment(new Comment("Jake", "This video is amazing!"));
+        video3.AddComment(new Comment("Adriel", "I learned a lot."));
+        video3.AddComment(new Comment("Nicolas", "Best video ever!"));
+        var videos = new List<Video> { video1, video2, video3 };
+
+        foreach (var video in videos)
+        {
+            video.DisplayVideo();
+            Console.WriteLine();
+        }
     }
 }
