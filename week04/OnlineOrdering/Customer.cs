5476e00eafb211c57fb9aab94300f7b8bf319c8a
Skyyzg15 <cielozegarragarcia@gmail.com>
2025-10-15 14:50:08 -0500

    finished homework

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+public class Customer 
+{
+    public string _name;
+    public Address _address;
+}
---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Product
+{
+    public string _productName;
+    public int _productId;
+    public int _price;
+    public int _quantity;
+
+    public Product(string productName, int productId, int price, int quantity)
+    {
+        _productName = productName;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+}
