7b795bbd4c05acff1a3887ef241612ed9bc7aeac
Skyyzg15 <cielozegarragarcia@gmail.com>
2025-10-16 17:08:33 -0500

    Mindfulness program done

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.",50)
+    {
+    }
+
+    public override void Run()
+    {
+        DisplayStartingMessage();
+        int cycles = _duration / 6;
+
+        for (int i = 0; i < cycles; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountdown(3);
+            Console.WriteLine("Breathe out...");
+            ShowCountdown(3);
+        } 
+        DisplayEndingMessage();
+    }
+}
